04e8100d59ebc1aa3db75e4dfdaf9cf92589ee92
phw726 <myphw726@gmail.com>
2024-05-06 23:52:06 +0900

    feat : 페이지세팅2

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,3 +1,12 @@
+import Footer from 'src/components/Footer';
+import Header from 'src/components/Header';
+
 export default function ProfilePage() {
-  return <>프로필페이지</>;
+  return (
+    <>
+      <Header />
+      마이페이지
+      <Footer />
+    </>
+  );
 }
